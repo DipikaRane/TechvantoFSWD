4cb697cfd2bea8bbcc4608023e125d68bc27f33c
DipikaRane <deepikajoge@gmail.com>
2022-03-25 12:51:15 +0530

    Friday 25March

---
+++ HTML/firstjs.js
@@ -0,0 +1,9 @@
+function hidediv(){
+    var mydiv=document.getElementById('coupon');
+    mydiv.style.display="none";
+}
+
+function showdiv(){
+    var mydiv=document.getElementById('coupon');
+    mydiv.style.display="block";
+}
