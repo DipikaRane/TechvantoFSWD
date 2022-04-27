ed6602e3278fcfc407ea45c5c6f3dc7c60d0f9df
DipikaRane <deepikajoge@gmail.com>
2022-04-27 18:19:32 +0530

    javascrpt

---
+++ Javascript/date27april2022.js
@@ -0,0 +1,113 @@
+es5
+
+var a = 10
+//keyword variable value
+var b = "Hii"
+var c = true
+
+es6
+let -- improved version of var
+const -- can not be updated or redeclared
+
+var a = 20
+var b = "Hello world"
+var c = true
+
+var h1 =20 --- valid
+
+var a = 10
+var b = 20
+
+a+b
+30
+a-b
+-10
+a*b
+200
+a/b
+0.5
+
+var a = 200
+var b = 10
+
+a%b
+0
+var a = 10
+var b = 20
+undefined
+
+## concatenation ## concat
+ var a = "Hiii"
+ var b = "Javascript"
+
+ a+b
+'HiiiJavascript'
+a/b
+NaN
+a*b
+NaN
+a-b
+Nan ---- Not a Number
+
+string + string = string  
+string + number = string
+number + string = string 
+number + number = number
+
+var a ="HII"
+var b = 2
+a+b
+'HII2'
+
+var a =10
+var b ="HII"
+
+var a =10
+var b =20
+a+b
+30
+
+string + string = string  
+string + number = string
+number + string = string 
+number + number = number
+
+"10"+20+30
+"1020"+30
+"102030"
+
+10+"20"+30
+"1020"+30
+"102030"
+
+10+20+"30"
+30+"30"
+"3030"
+
+10+20+"30"-1
+30
+
+if both sides of an operator there purely number then it perform arithmatic operations
+but + will concat.
+
+"10"+"1"
+'101'
+"10"-"1"
+9
+
+isNaN(1)
+false
+isNaN('2')
+false
+isNaN("jhs")
+true
+
+truthy
+# any number +ve or -ve except 0 is truthy
+any string is truthy
+
+falsy
+0
+false
+null or undefined
+
