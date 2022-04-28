fe04aedacb3c420ca2ec19bdb0ecbbe9663666c9
DipikaRane <deepikajoge@gmail.com>
2022-04-28 18:38:28 +0530

    DOM

---
+++ Javascript/DOM.js
@@ -0,0 +1,16 @@
+//DOM -Document object model 
+
+document
+document.getElementsByTagName('p')
+document.getElementsByTagName('p')[0]
+document.getElementsByTagName('p')[0].innerText
+document.getElementsByTagName('p')[0].innerText="Contact Us"
+
+document.getElementsByClassName('btn-warning')[0]
+<button type=​"button" class=​"btn btn-warning btn-block">​Submit​</button>​
+document.getElementsByClassName('btn-warning')[1]
+<a class=​"btn btn-warning" href=​"/​selectone/​3">​Purchase​</a>​
+document.getElementsByClassName('btn-warning')[1].style.color="red"
+'red'
+document.getElementsByClassName('btn-warning')[1].style.display="none"
+'none'
--- Javascript/date27april2022.js
+++ Javascript/date27april2022.js
@@ -1,19 +1,19 @@
-es5
+//es5
 
 var a = 10
 //keyword variable value
 var b = "Hii"
 var c = true
 
-es6
-let -- improved version of var
-const -- can not be updated or redeclared
+//es6
+let q=5//-- improved version of var
+const a=3 //-- can not be updated or redeclared
 
 var a = 20
 var b = "Hello world"
 var c = true
 
-var h1 =20 --- valid
+var h1 =20 //--- valid
 
 var a = 10
 var b = 20
@@ -36,7 +36,7 @@ var a = 10
 var b = 20
 undefined
 
-## concatenation ## concat
+//## concatenation ## concat
  var a = "Hiii"
  var b = "Javascript"
 
@@ -47,12 +47,12 @@ NaN
 a*b
 NaN
 a-b
-Nan ---- Not a Number
+NaN //---- Not a Number
 
-string + string = string  
-string + number = string
-number + string = string 
-number + number = number
+//string + string = string 
+//string + number = string
+//number + string = string 
+//number + number = number
 
 var a ="HII"
 var b = 2
@@ -67,10 +67,10 @@ var b =20
 a+b
 30
 
-string + string = string  
-string + number = string
-number + string = string 
-number + number = number
+//string + string = string  
+//string + number = string
+//number + string = string 
+//number + number = number
 
 "10"+20+30
 "1020"+30
@@ -87,8 +87,8 @@ number + number = number
 10+20+"30"-1
 30
 
-if both sides of an operator there purely number then it perform arithmatic operations
-but + will concat.
+//if both sides of an operator there purely number then it perform arithmatic operations
+//but + will concat.
 
 "10"+"1"
 '101'
@@ -102,12 +102,12 @@ false
 isNaN("jhs")
 true
 
-truthy
-# any number +ve or -ve except 0 is truthy
-any string is truthy
+//truthy
+//# any number +ve or -ve except 0 is truthy
+//any string is truthy
 
-falsy
+//falsy
 0
 false
-null or undefined
+null / undefined
 
