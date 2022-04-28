99dee9395061c70cf185107ebe9393683ef82976
DipikaRane <deepikajoge@gmail.com>
2022-04-28 18:41:02 +0530

    js

---
+++ Javascript/date28april2022.js
@@ -0,0 +1,66 @@
+//Boolean
+var a=true;
+var b=true;
+var c=false;
+
+a+b //true+true =2 (1+1)
+
+10+true
+
+var a=true;
+var b=true;
+var c=false;
+undefined
+a+b
+2
+a+c
+1
+10+true
+11
+10/false
+Infinity
+"hii"+false
+'hiifalse'
+"hii"-false
+NaN
+
+var a="10"
+var b="20"
+
+//es5
+
+parseInt(a)+parseInt(b)
+30
+
+parseInt
+parseFloat
+
+var a="10.11"
+var b="20"
+parseInt(a)+parseInt(b)
+
+var a = "hii"
+parseInt(a)
+
+var a = "Delhi123"
+var b = "123Delhi344"
+parseInt(a)
+parseInt(b)
+
+//es6
+Number()
+
+var a="10"
+Number(a)
+10
+
+var a="10.11"
+Number(a)
+10.11
+
+var a="hhiii"
+Number(a)
+NaN
+
+var a="10a"
+var a="a10a"
---
+++ Javascript/date28april22.js
@@ -0,0 +1,38 @@
+//Comparison
+// there are 3 comparison operators
+
+=  //assignment
+== //Comparison operator comapre only data
+=== // compare data and datatype
+
+var k=100
+var l="100"
+undefined
+k==l
+true
+k===l
+false
+var k="jhon"
+undefined
+var l="John"
+undefined
+k==l
+false
+k===l
+false
+
+console.log()
+alert() //notification
+prompt() //taking input
+confirm() //yes or no
+
+var first=prompt("enter first value")
+var second=prompt("enter second value")
+first+second
+'1223'
+
+var first=prompt("enter first value")
+var second=prompt("enter second value")
+parseFloat(first)+parseFloat(second)
+46.11
+
